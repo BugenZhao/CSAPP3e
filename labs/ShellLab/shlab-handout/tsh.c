8b9b9fb54d5cb8ffdf8bdafa6f44473a3c0b32a8
BugenZhao <bugenzhao@qq.com>
2020-02-15 18:38:44 +0800

    Shell Lab (3/16)

--- labs/ShellLab/shlab-handout/tsh.c
+++ labs/ShellLab/shlab-handout/tsh.c
@@ -183,10 +183,28 @@ int main(int argc, char **argv) {
 */
 void eval(char *cmdline) {
     char *argv[MAXARGS];
-    int bg_flag;
-    bg_flag = parseline(cmdline, argv);
-    if (builtin_cmd(argv)) {
-        return;
+    char buf[MAXLINE];
+    int bg;
+    pid_t pid;
+
+    strcpy(buf, cmdline);
+    bg = parseline(buf, argv);
+    if (argv[0] == NULL) return;
+
+    if (!builtin_cmd(argv)) {
+        if ((pid = fork()) == 0) {
+            if (execve(argv[0], argv, environ) < 0) {
+                printf("%s: Command not found.\n", argv[0]);
+                exit(0);
+            }
+        }
+        if (!bg) {
+            int status;
+            if (waitpid(pid, &status, 0) < 0)
+                unix_error("waitfg: waitpid error");
+        } else {
+            printf("%d %s", pid, cmdline);
+        }
     }
     return;
 }
