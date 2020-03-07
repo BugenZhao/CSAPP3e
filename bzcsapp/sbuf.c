e6bc058d0d6051e5d7640a133491d89087e4a98c
BugenZhao <bugenzhao@qq.com>
2020-03-07 12:09:30 +0800

    Update CMake flags

--- bzcsapp/sbuf.c
+++ bzcsapp/sbuf.c
@@ -11,9 +11,9 @@ void sbuf_init(sbuf_t *sp, int n) {
     sp->buf = Calloc(n, sizeof(int));
     sp->n = n;                       /* Buffer holds max of n items */
     sp->front = sp->rear = 0;        /* Empty buffer iff front == rear */
-    sp->mutex = *Sem_open_and_unlink("/_mutex_", 1);
-    sp->slots = *Sem_open_and_unlink("/_slots_", n);
-    sp->items = *Sem_open_and_unlink("/_items_", 0);
+    sp->mutex = Sem_open_and_unlink("/_mutex_", 1);
+    sp->slots = Sem_open_and_unlink("/_slots_", n);
+    sp->items = Sem_open_and_unlink("/_items_", 0);
 //    Sem_init(&sp->mutex, 0, 1);      /* Binary semaphore for locking */
 //    Sem_init(&sp->slots, 0, n);      /* Initially, buf has n empty slots */
 //    Sem_init(&sp->items, 0, 0);      /* Initially, buf has zero data items */
@@ -30,11 +30,11 @@ void sbuf_deinit(sbuf_t *sp) {
 /* Insert item onto the rear of shared buffer sp */
 /* $begin sbuf_insert */
 void sbuf_insert(sbuf_t *sp, int item) {
-    P(&sp->slots);                          /* Wait for available slot */
-    P(&sp->mutex);                          /* Lock the buffer */
+    P(sp->slots);                          /* Wait for available slot */
+    P(sp->mutex);                          /* Lock the buffer */
     sp->buf[(++sp->rear) % (sp->n)] = item;   /* Insert the item */
-    V(&sp->mutex);                          /* Unlock the buffer */
-    V(&sp->items);                          /* Announce available item */
+    V(sp->mutex);                          /* Unlock the buffer */
+    V(sp->items);                          /* Announce available item */
 }
 /* $end sbuf_insert */
 
@@ -42,11 +42,11 @@ void sbuf_insert(sbuf_t *sp, int item) {
 /* $begin sbuf_remove */
 int sbuf_remove(sbuf_t *sp) {
     int item;
-    P(&sp->items);                          /* Wait for available item */
-    P(&sp->mutex);                          /* Lock the buffer */
+    P(sp->items);                          /* Wait for available item */
+    P(sp->mutex);                          /* Lock the buffer */
     item = sp->buf[(++sp->front) % (sp->n)];  /* Remove the item */
-    V(&sp->mutex);                          /* Unlock the buffer */
-    V(&sp->slots);                          /* Announce available slot */
+    V(sp->mutex);                          /* Unlock the buffer */
+    V(sp->slots);                          /* Announce available slot */
     return item;
 }
 /* $end sbuf_remove */
--- bzcsapp/sbuf.h
+++ bzcsapp/sbuf.h
@@ -13,9 +13,9 @@ typedef struct {
     int n;             /* Maximum number of slots */
     int front;         /* buf[(front+1)%n] is first item */
     int rear;          /* buf[rear%n] is last item */
-    sem_t mutex;       /* Protects accesses to buf */
-    sem_t slots;       /* Counts available slots */
-    sem_t items;       /* Counts available items */
+    sem_t * mutex;       /* Protects accesses to buf */
+    sem_t *slots;       /* Counts available slots */
+    sem_t *items;       /* Counts available items */
 } sbuf_t;
 
 /* $end sbuft */
