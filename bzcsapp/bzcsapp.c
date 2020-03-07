eee85d36baa93005b899d5ec8acee1200e646683
BugenZhao <bugenzhao@qq.com>
2020-03-07 09:06:00 +0800

    Semaphore for macOS

--- bzcsapp/bzcsapp.c
+++ bzcsapp/bzcsapp.c
@@ -9,7 +9,14 @@ sem_t *Sem_open_and_unlink(const char *name, unsigned int value) {
     sem_t *sem;
     if ((sem = sem_open(name, O_CREAT | O_EXCL, 0600, value)) < 0)
         unix_error("Semaphore open failed");
-    if(sem_unlink(name)<0)
+    if (sem_unlink(name) < 0)
         unix_error("Semaphore unlink failed");
     return sem;
+}
+
+int Sem_close(sem_t *sem) {
+    int ret;
+    if ((ret = sem_close(sem)) < 0)
+        unix_error("Semaphore close failed");
+    return ret;
 }
--- bzcsapp/bzcsapp.h
+++ bzcsapp/bzcsapp.h
@@ -9,4 +9,6 @@
 
 sem_t * Sem_open_and_unlink(const char *name, unsigned int value);
 
+int Sem_close(sem_t *sem);
+
 #endif //BUGENCSAPP3E_BZCSAPP_H
