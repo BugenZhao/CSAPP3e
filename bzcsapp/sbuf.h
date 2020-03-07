27ee0040029e35a9a7ef001f66f943f60161896d
BugenZhao <bugenzhao@qq.com>
2020-03-07 17:46:34 +0800

    Update semaphore for macOS

--- bzcsapp/sbuf.h
+++ bzcsapp/sbuf.h
@@ -13,7 +13,7 @@ typedef struct {
     int n;             /* Maximum number of slots */
     int front;         /* buf[(front+1)%n] is first item */
     int rear;          /* buf[rear%n] is last item */
-    sem_t * mutex;       /* Protects accesses to buf */
+    sem_t *mutex;       /* Protects accesses to buf */
     sem_t *slots;       /* Counts available slots */
     sem_t *items;       /* Counts available items */
 } sbuf_t;
